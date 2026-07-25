8f9018ec7f6a441d8f48218ce6dc73ec776625ac
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-07-25 21:19:13 +0400

    Доработан класс WordLoader

--- Hangman Console/src/core/WordLoader.java
+++ Hangman Console/src/core/WordLoader.java
@@ -7,9 +7,24 @@ import java.util.List;
 public class WordLoader {
     private final List<String> _words;
 
+    public WordLoader() {
+        _words = new ArrayList<>();
+    }
+
     public WordLoader(String filePath) {
         _words = new ArrayList<>();
+        load(filePath);
+    }
 
+    public String getWord(int index){
+        return _words.get(index);
+    }
+
+    public int getWordsLength(){
+        return _words.size();
+    }
+
+    public void load(String filePath){
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -20,13 +35,5 @@ public class WordLoader {
         } catch (IOException e) {
             System.out.println("Ошибка при чтении файла: " + e.getMessage());
         }
-    }
-
-    public String getWord(int index){
-        return _words.get(index);
-    }
-
-    public int getWordsLength(){
-        return _words.size();
     }
 }
