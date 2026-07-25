d7a7e2ea6f529fb2f601bfcd0a1a22d10bb9ec2b
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-07-25 21:51:08 +0400

    Доработка класса Menu

---
+++ Hangman Console/src/core/Game.java
@@ -0,0 +1,9 @@
+package core;
+
+public class Game {
+    public Game(){}
+
+    public void start(){
+        System.out.println("...начало игры");
+    }
+}
--- Hangman Console/src/core/Menu.java
+++ Hangman Console/src/core/Menu.java
@@ -1,24 +1,34 @@
 package core;
 
 import java.util.*;
+
+import io.InputHandler;
+import io.Printer;
+import model.InputType;
 import model.MenuItem;
 
 public class Menu {
     private final String _text;
-    private List<MenuItem> _items;
+    private final List<MenuItem> _items;
+    private final Printer _printer;
+    private final InputHandler _input;
+
 
     public Menu(String text, MenuItem... items){
         _text = text;
-        for(MenuItem item : items){
-            _items.add(item);
+        _items = new ArrayList<>();
+        _printer = new Printer();
+        _input = new InputHandler();
+        Collections.addAll(_items, items);
+    }
+
+    public void start(){
+        _printer.println(_text);
+        for(MenuItem item : _items){
+            _printer.println(item.getText());
         }
-    }
 
-    public String getText(){
-        return _text;
-    }
-
-    public List<MenuItem> getItems(){
-        return _items;
+        int selectedItem = _input.getInput("Выберите пункт меню: ", InputType.Number);
+        _items.get(selectedItem - 1).choose();
     }
 }
