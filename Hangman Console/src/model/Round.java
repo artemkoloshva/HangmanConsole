b9ad90c4fba167adaf56cf7493e942df5cd6c853
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-07-26 17:31:37 +0400

    Доработка Round

--- Hangman Console/src/model/Round.java
+++ Hangman Console/src/model/Round.java
@@ -2,11 +2,13 @@ package model;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Round {
-    private String _word;
-    private Set<Character> _letters;
+    private List<Character> _word;
+    private Set<Character> _errorLetters; // Заменили List на Set
+    private Set<Character> _usedLetters;
     private int _errorsCount;
     private boolean _isWin;
 
@@ -15,37 +17,70 @@ public class Round {
     }
 
     public void clear(){
-        _word = "";
-        _letters = new HashSet<>();
+        _word = new ArrayList<>();
+        _errorLetters = new HashSet<>(); // Инициализируем Set
+        _usedLetters = new HashSet<>();
         _errorsCount = 0;
         _isWin = false;
     }
 
     public void setWord(String word){
-        _word = word;
+        for(Character letter : word.toCharArray()){
+            _word.add(letter);
+        }
     }
 
-    public String getWord(){
-        return _word;
+    public Character getWordLetter(int index){
+        return _word.get(index);
     }
 
-    public void addLetter(Character letter){
-        _letters.add(letter);
+    public List<Character> getWordLetters(){
+        return new ArrayList<>(_word);
+    }
+
+    public int getWordLength(){
+        return _word.size();
     }
 
     public boolean hasLetter(Character letter){
-        return _letters.contains(letter);
+        for (Character let : _word){
+            if(let.equals(letter)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addUsedLetter(Character letter){
+        _usedLetters.add(letter);
+    }
+
+    public boolean isUsedLetter(Character letter){
+        return _usedLetters.contains(letter);
+    }
+
+    public Set<Character> getUsedLetters(){
+        return new HashSet<>(_usedLetters);
+    }
+
+    public Set<Character> getErrorLetters(){
+        return new HashSet<>(_errorLetters);
     }
 
     public int getErrors(){
         return _errorsCount;
     }
 
-    public void addError(){
+    public void addError(Character letter){
+        _errorLetters.add(letter);
         _errorsCount++;
     }
 
     public boolean isWin(){
         return _isWin;
+    }
+
+    public void setWin(boolean win){
+        _isWin = win;
     }
 }
