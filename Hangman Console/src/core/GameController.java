f865cae382090a216f50ab920e8daa06c0a987a9
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-07-26 14:14:23 +0400

    Доработка контроллера игры

--- Hangman Console/src/core/GameController.java
+++ Hangman Console/src/core/GameController.java
@@ -1,10 +1,7 @@
 package core;
 
-import io.InputHandler;
-import io.Printer;
-import model.InputType;
-import model.Round;
-
+import io.*;
+import model.*;
 import java.util.Random;
 
 public class GameController {
@@ -20,21 +17,35 @@ public class GameController {
         _round = new Round();
     }
 
-    public String getRandomWord(){
-        Random random = new Random();
-        return _wordLoader.getWord(random.nextInt(_wordLoader.getWordsLength()));
-    }
-
     public void newRound(){
         _round.clear();
         _round.setWord(getRandomWord());
     }
 
+    public String getRandomWord(){
+        Random random = new Random();
+        return _wordLoader.getWord(random.nextInt(_wordLoader.getWordsLength()));
+    }
+
+    public String getArtByErrors(int error){
+        return switch (_round.getErrors()) {
+            case 0 -> HangmanASCII.Stage0.getArt();
+            case 1 -> HangmanASCII.Stage1.getArt();
+            case 2 -> HangmanASCII.Stage2.getArt();
+            case 3 -> HangmanASCII.Stage3.getArt();
+            case 4 -> HangmanASCII.Stage4.getArt();
+            case 5 -> HangmanASCII.Stage5.getArt();
+            default -> HangmanASCII.Stage6.getArt();
+        };
+    }
+
+    public
+
     public void start(){
         newRound();
         _printer.println(_round.getWord());
         while (!_round.isWin()){
-            _printer.println("[РИСУНОК ВИСЕЛИЦЫ]");
+            _printer.println(getArtByErrors(_round.getErrors()));
             _printer.println("Cлово: _ _ _ _ _ _ _");
             _printer.println("Ошибки ("+_round.getErrors()+"): п, в, е, а");
             _input.getInput("Введите букву: ", InputType.Letter);
