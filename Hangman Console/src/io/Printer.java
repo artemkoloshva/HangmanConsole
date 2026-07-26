e3da0dad919d1d22c30d41f4408bce74a51f9f65
artemkoloshva <artemkoloshva11yt@gmail.com>
2026-07-26 17:31:49 +0400

    Доработка Printer

--- Hangman Console/src/io/Printer.java
+++ Hangman Console/src/io/Printer.java
@@ -1,5 +1,10 @@
 package io;
 
+import model.HangmanASCII;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Printer {
     public Printer(){}
 
@@ -9,5 +14,43 @@ public class Printer {
 
     public void print(String text){
         System.out.print(text);
+    }
+
+    public void printHangmanArt(int errors) {
+        String art = switch (errors) {
+            case 0 -> HangmanASCII.Stage0.getArt();
+            case 1 -> HangmanASCII.Stage1.getArt();
+            case 2 -> HangmanASCII.Stage2.getArt();
+            case 3 -> HangmanASCII.Stage3.getArt();
+            case 4 -> HangmanASCII.Stage4.getArt();
+            case 5 -> HangmanASCII.Stage5.getArt();
+            default -> HangmanASCII.Stage6.getArt();
+        };
+        System.out.println(art);
+    }
+
+    public void printDisplayWord(List<Character> wordLetters, Set<Character> usedLetters) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < wordLetters.size(); i++) {
+            Character letter = wordLetters.get(i);
+
+            if (usedLetters.contains(letter)) {
+                char displayChar = (i == 0) ? Character.toUpperCase(letter) : letter;
+                result.append(displayChar).append(" ");
+            } else {
+                result.append("_ ");
+            }
+        }
+        System.out.println("Слово: " + result.toString().trim());
+    }
+
+    public void printErrorLetters(int errorsCount, Set<Character> errorLetters) {
+        String lettersStr = "";
+        if (!errorLetters.isEmpty()) {
+            lettersStr = errorLetters.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(" "));
+        }
+        System.out.println("Ошибки (" + errorsCount + "): " + lettersStr);
     }
 }
